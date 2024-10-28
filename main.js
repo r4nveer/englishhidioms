079da51bd312ae3b63e4f7f57dae1e437d6ea822
r4nveer <r.dhillon12@share.epsb.ca>
2024-10-28 14:23:55 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// Pizza Order by Mr. V
+
+// Add an Event Listener to the button to run the placeOrder function
+document.getElementById("btn").addEventListener("click", placeOrder);
+document.getElementById("btn2").addEventListener("click", Jatt);
+document.getElementById("btn3").addEventListener("click", Vailley);
+document.getElementById("btn4").addEventListener("click", buggay);
+document.getElementById("btn5").addEventListener("click", sig);
+// Store order code in a function
+function placeOrder() {
+  // INPUT
+  let size = alert("..bush ");
+}
+
+function Jatt() {
+  let size = alert("..leg");
+}
+
+function Vailley() {
+  let size = alert("..worlds");
+}
+
+function buggay() {
+  let size = alert("..weather");
+}
+
+function sig() {
+  let size = alert("..devil");
+}
